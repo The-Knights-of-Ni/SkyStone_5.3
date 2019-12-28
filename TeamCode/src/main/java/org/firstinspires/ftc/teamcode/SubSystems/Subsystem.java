b5ef5d18b941b4646c541aa0e413cc2361f811db
Overlord999 <32445772+Overlord999@users.noreply.github.com>
2019-12-27 17:56:42 -0800

    Update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Subsystem.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.SubSystems;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/** Abstract superclass to all subsystems */
+public abstract class Subsystem {
+    protected ElapsedTime timer;
+}
