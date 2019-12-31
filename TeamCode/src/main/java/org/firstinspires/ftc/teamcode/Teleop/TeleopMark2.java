a3af29f1665af88b133fe744f62fe0c8a4081e68
Paige Yeung <paigeyeung@gmail.com>
2019-12-30 16:23:19 -0800

    commented out foundation servo testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleopMark2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleopMark2.java
@@ -197,16 +197,16 @@ public class TeleopMark2 extends LinearOpMode {
 //            telemetry.addData("Right Rear Power", robot.rearRightDriveMotor.getPower());
 //            telemetry.addData("Right Front Power", robot.frontRightDriveMotor.getPower());
 
-            // add servos
-
-            robot.fClawL.setPosition(this.gamepad2.left_stick_x*0.5+0.5);
-            robot.fClawR.setPosition(this.gamepad2.right_stick_x*0.5+0.5);
-
-            telemetry.addData("LEFT Foundation Claw Servo Position", robot.fClawL.getPosition());
-            telemetry.addData("Status", "Running");
-
-            telemetry.addData("RIGHT Foundation Claw Servo Position", robot.fClawR.getPosition());
-            telemetry.addData("Status", "Running");
+//            // add servos
+//
+//            robot.fClawL.setPosition(this.gamepad2.left_stick_x*0.5+0.5);
+//            robot.fClawR.setPosition(this.gamepad2.right_stick_x*0.5+0.5);
+//
+//            telemetry.addData("LEFT Foundation Claw Servo Position", robot.fClawL.getPosition());
+//            telemetry.addData("Status", "Running");
+//
+//            telemetry.addData("RIGHT Foundation Claw Servo Position", robot.fClawR.getPosition());
+//            telemetry.addData("Status", "Running");
 
             telemetry.update();
             timePre = timeCurrent;
