4bdec64bb62e3d9d759964d97119fcf735ac5412
Overlord999 <32445772+Overlord999@users.noreply.github.com>
2019-12-30 16:26:02 -0800

    Update AutoEncoder.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoEncoder.java
@@ -76,13 +76,13 @@ public class AutoEncoder extends LinearOpMode {
         telemetry.update();
         waitForStart();
 
-        double ratio = 0.2;
-
-        robot.drive.frontLeft.setVelocity(robot.drive.getAngularVMaxNeverrest20() * ratio);
-        robot.drive.rearLeft.setVelocity((-robot.drive.getAngularVMaxNeverrest20() * ratio));
-        robot.drive.frontRight.setVelocity((-robot.drive.getAngularVMaxNeverrest20()) * ratio);
-        robot.drive.rearRight.setVelocity((robot.drive.getAngularVMaxNeverrest20()) * ratio);
-        sleep(10000);
+//        double ratio = 0.2;
+//
+//        robot.drive.frontLeft.setVelocity(robot.drive.getAngularVMaxNeverrest20() * ratio);
+//        robot.drive.rearLeft.setVelocity((-robot.drive.getAngularVMaxNeverrest20() * ratio));
+//        robot.drive.frontRight.setVelocity((-robot.drive.getAngularVMaxNeverrest20()) * ratio);
+//        robot.drive.rearRight.setVelocity((robot.drive.getAngularVMaxNeverrest20()) * ratio);
+//        sleep(10000);
 
 
 
