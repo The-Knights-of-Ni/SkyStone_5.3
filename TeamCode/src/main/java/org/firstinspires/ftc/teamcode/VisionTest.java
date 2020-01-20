2c18df3017c1a3a59e816b37e7a6b4656fdcf44f
Overlord999 <32445772+Overlord999@users.noreply.github.com>
2020-01-20 09:22:39 -0800

    restore camera test programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTest.java
@@ -20,55 +20,27 @@ public class VisionTest extends LinearOpMode {
 
     private Robot robot;
 
-    boolean aButton2;
-    boolean isaButton2PressedPrev = false;
-
     double timeCurrent;
+
+    // cameraSelection : 0 (front webcam), 1 (arm webcam)
+    private int cameraSelection = 0;
 
     public void initOpMode(){
         ElapsedTime timer = new ElapsedTime();
+        // visionMode 3: backWebcam is initialized for Vuforia and frontWebcam is initialized for OpenCV
         this.robot = new Robot(this, timer, 3);
-
+        cameraSelection = 0;
     }
     public void runOpMode() {
         ElapsedTime timer = new ElapsedTime();
         initOpMode();
         waitForStart();
         robot.vision.getTargetsSkyStone().activate();
-        isaButton2PressedPrev = false;
         while (!isStopRequested()) {
 
-//            // check all the trackable targets to see which one (if any) is visible.
-//            robot.vision.changeIsTargetVisible(false);
-//            for (VuforiaTrackable trackable : robot.vision.getAllTrackables()) {
-//                if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
-//                    telemetry.addData("Visible Target", trackable.getName());
-//                    robot.vision.changeIsTargetVisible(true);
-//
-//                    // getUpdatedRobotLocation() will return null if no new information is available since
-//                    // the last time that call was made, or if the trackable is not currently visible.
-//                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
-//                    if (robotLocationTransform != null) {
-//                        robot.vision.changeLastLocation(robotLocationTransform);
-//                    }
-//                    break;
-//                }
-//            }
-//            // Provide feedback as to where the robot is located (if we know).
-//            if (robot.vision.isTargetVisible()) {
-//                // express position (translation) of robot in inches.
-//                VectorF translation = robot.vision.getLastLocation().getTranslation();
-//                telemetry.addData("Pos (mm)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-//                        translation.get(0), translation.get(1), translation.get(2));
-//
-//                // express the rotation of the robot in degrees.
-//                Orientation rotation = Orientation.getOrientation(robot.vision.getLastLocation(), EXTRINSIC, XYZ, DEGREES);
-//                telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
-//            }
-//            else {
-//                telemetry.addData("Visible Target", "none");
-//            }
-//            telemetry.update();
+            // Get gamepad inputs
+            robot.getGamePadInputs();
+
 
             robot.vision.vuMarkScan();
             robot.getOpmode().telemetry.addData("Threshold", "{r, b, y1, y2} = %d, %d, %d, %d",
@@ -76,21 +48,33 @@ public class VisionTest extends LinearOpMode {
                     robot.vision.getYellowColorThreshold1(), robot.vision.getYellowColorThreshold2());
 
             timeCurrent = timer.nanoseconds();
-            aButton2 = gamepad2.a;
 
             // save images
-            if (aButton2 && !isaButton2PressedPrev) {
+            if (robot.aButton2 && !robot.isaButton2PressedPrev) {
                 robot.getOpmode().telemetry.addData("saving ", "images...");
                 robot.vision.saveImage("VisionTest", robot.vision.frameBuffer1, Imgproc.COLOR_RGBA2BGR, "original", (long) timeCurrent);
                 robot.vision.saveImage("VisionTest", robot.vision.yCbCrChan2Mat, Imgproc.COLOR_RGBA2BGR, "CbImage", (long) timeCurrent);
                 robot.vision.saveImage("VisionTest", robot.vision.yCbCrChan1Mat, Imgproc.COLOR_RGBA2BGR, "CrImage", (long) timeCurrent);
                 robot.vision.saveImage("VisionTest", robot.vision.thresholdMat, Imgproc.COLOR_RGBA2BGR, "threshold", (long) timeCurrent);
                 robot.vision.saveImage("VisionTest", robot.vision.contoursOnFrameMat, Imgproc.COLOR_RGBA2BGR, "contours", (long) timeCurrent);
-                isaButton2PressedPrev = true;
             }
-            if (!aButton2) {
-                isaButton2PressedPrev = false;
+
+            // toggle cameras
+            if (robot.bButton2 && !robot.isbButton2PressedPrev) {
+                if (cameraSelection == 0) {
+                    robot.getOpmode().telemetry.addData("camera selected: ", "front webcam");
+                }
+                else {
+                    robot.getOpmode().telemetry.addData("camera selected: ", "arm webcam");
+                }
             }
+            if (cameraSelection == 0) {
+                robot.getOpmode().telemetry.addData("camera selected: ", "front webcam");
+            }
+            else {
+                robot.getOpmode().telemetry.addData("camera selected: ", "arm webcam");
+            }
+
             robot.getOpmode().telemetry.update();
 
         }
